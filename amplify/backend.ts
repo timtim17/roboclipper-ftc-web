b955cff2be8dad3fa0bf571918b43dee00f2a1ee
Austin Jenchi <ajenchi9@gmail.com>
2024-11-13 00:23:03 -0800

    Update auth params

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,6 +4,23 @@ import { auth } from './auth/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
 });
+
+const { cfnIdentityPool, cfnUserPool } = backend.auth.resources.cfnResources;
+cfnIdentityPool.allowUnauthenticatedIdentities = false;
+cfnUserPool.adminCreateUserConfig = {
+    ...cfnUserPool.adminCreateUserConfig,
+    allowAdminCreateUserOnly: true,
+};
+cfnUserPool.policies = {
+    passwordPolicy: {
+        minimumLength: 8,
+        requireLowercase: true,
+        requireNumbers: false,
+        requireSymbols: true,
+        requireUppercase: true,
+        temporaryPasswordValidityDays: 365,
+    },
+};
