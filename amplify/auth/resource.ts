c132dcf4bb03f6a510e1186edd034e6dd9b6832c
Austin Jenchi <ajenchi9@gmail.com>
2024-11-14 01:26:47 -0800

    Update amplify auth email template

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,11 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+        userInvitation: {
+            emailSubject: 'RoboClipper for FTCLive - Account Created',
+            emailBody: (user, code) => `An account was created for you for RoboClipper.<br>https://clip.ftc.tools<br>Login with username ${user()} and temporary password ${code()}.`,
+        },
+    },
   },
 });
